d94f25948e7efcc5126d71b7e78a3360d09c3708
sofyan <soufianeboukir0@gmail.com>
2025-07-19 00:28:42 +0100

    add suspense component to avoid collecting page data fail

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,10 +1,12 @@
 import { LoginForm } from '@/components/login-form'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 const page = () => {
     return (
         <div className='bg-muted flex min-h-svh items-center justify-center gap-6 p-6 md:p-10'>
-            <LoginForm />
+            <Suspense>
+                <LoginForm />
+            </Suspense>
         </div>
     )
 }
--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -1,10 +1,12 @@
 import ResetPasswordForm from '@/components/reset-password-form';
-import React from 'react'
+import React, { Suspense } from 'react'
 
 const page = () => {
     return (
         <div className='bg-muted flex min-h-svh items-center justify-center gap-6 p-6 md:p-10'>
-            <ResetPasswordForm />
+            <Suspense>
+                <ResetPasswordForm />
+            </Suspense>
         </div>
     )
 }
